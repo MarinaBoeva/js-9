a918981ed54f214093af46d0697e8297b785659e
Marina Boeva <camado6pota@yandex.ru>
2016-09-07 01:15:01 +0300

    new checkbox

--- js/checkbox.js
+++ js/checkbox.js
@@ -1,7 +1,19 @@
 jQuery(document).ready(function(){
 
+jQuery(".niceCheck").mousedown(
+/* при клике на чекбоксе меняем его вид и значение */
+function() {
+     changeCheck(jQuery(this));
+});
+jQuery("label").mousedown(
+/* при клике на чекбоксе меняем его вид и значение */
+function() {
+     changeCheck(jQuery(this).next('.niceCheck'));
+});
+
+
 jQuery(".niceCheck").each(
-/* при загрузке страницы меняем обычные на стильные checkbox */
+/* при загрузке страницы нужно проверить какое значение имеет чекбокс и в соответствии с ним выставить вид */
 function() {
 
      changeCheckStart(jQuery(this));
@@ -10,99 +22,34 @@ function() {
 
                                         });
 
-
 function changeCheck(el)
 /*
-	функция смены вида и значения чекбокса при клике на контейнер чекбокса (тот, который отвечает за новый вид)
-	el - span контейнер для обычного чекбокса
+	функция смены вида и значения чекбокса
+	el - span контейнер дял обычного чекбокса
 	input - чекбокс
 */
 {
-
-	var el = el,
-		input = el.find("input").eq(0);
-
-	if(el.attr("class").indexOf("niceCheckDisabled")==-1)
-	{
-   		if(!input.attr("checked")) {
-			el.addClass("niceChecked");
-			input.attr("checked", true);
-		} else {
-			el.removeClass("niceChecked");
-			input.attr("checked", false).focus();
-		}
+     var el = el,
+          input = el.find("input").eq(0);
+   	 if(!input.attr("checked")) {
+		el.css("background-position","0 -16px");
+		input.attr("checked", true);
+	} else {
+    input.attr("checked", false);
+		el.css("background-position","0 0");
 	}
-
-    return true;
-}
-
-function changeVisualCheck(input)
-{
-/*
-	меняем вид чекбокса при смене значения
-*/
-var wrapInput = input.parent();
-	if(!input.attr("checked")) {
-		wrapInput.removeClass("niceChecked");
-	}
-	else
-	{
-		wrapInput.addClass("niceChecked");
-	}
+     return true;
 }
 
 function changeCheckStart(el)
 /*
-	новый чекбокс выглядит так <span class="niceCheck"><input type="checkbox" name="[name check]" id="[id check]" [checked="checked"] /></span>
-	новый чекбокс получает теже name, id и другие атрибуты что и были у обычного
+	если установлен атрибут checked, меняем вид чекбокса
 */
 {
-
-try
-{
 var el = el,
-	checkName = el.attr("name"),
-	checkId = el.attr("id"),
-	checkChecked = el.attr("checked"),
-	checkDisabled = el.attr("disabled"),
-	checkTab = el.attr("tabindex"),
-    checkValue = el.attr("value");
-	if(checkChecked)
-		el.after("<span class='niceCheck niceChecked'>"+
-			"<input type='checkbox'"+
-			"name='"+checkName+"'"+
-			"id='"+checkId+"'"+
-			"checked='"+checkChecked+"'"+
-            "value='"+checkValue+"'"+
-			"tabindex='"+checkTab+"' /></span>");
-	else
-		el.after("<span class='niceCheck'>"+
-			"<input type='checkbox'"+
-			"name='"+checkName+"'"+
-			"id='"+checkId+"'"+
-             "value='"+checkValue+"'"+
-			"tabindex='"+checkTab+"' /></span>");
-
-	/* если checkbox disabled - добавляем соотвсмтвующи класс для нужного вида и добавляем атрибут disabled для вложенного chekcbox */
-	if(checkDisabled)
-	{
-		el.next().addClass("niceCheckDisabled");
-		el.next().find("input").eq(0).attr("disabled","disabled");
-	}
-
-	/* цепляем обработчики стилизированным checkbox */
-	el.next().bind("mousedown", function(e) { changeCheck(jQuery(this)) });
-	el.next().find("input").eq(0).bind("change", function(e) { changeVisualCheck(jQuery(this)) });
-	if(jQuery.browser.msie)
-	{
-		el.next().find("input").eq(0).bind("click", function(e) { changeVisualCheck(jQuery(this)) });
-	}
-	el.remove();
-}
-catch(e)
-{
-	// если ошибка, ничего не делаем
-}
-
-    return true;
+		input = el.find("input").eq(0);
+      if(input.attr("checked")) {
+		el.css("background-position","0 -16px");
+		}
+     return true;
 }
